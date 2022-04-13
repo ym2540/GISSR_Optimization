6d39e360e85565ae6e16dea839924b0ba1201932
Abel Valko <abel_valko@yahoo.com>
2022-04-13 15:42:22 -0400

    Added support for multiple divisions to ideal gissr test code

--- ideal_test.py
+++ ideal_test.py
@@ -5,36 +5,40 @@ import numpy as np
 
 topo_file = "ideal_topo_1.csv"
 storm_file = "ideal_storm_1.csv"
+div_data_file = "ideal_div_data.csv"
 
 topo = pd.read_csv(topo_file)
-roughness = topo["roughness"].to_numpy()
+div_data = pd.read_csv(div_data_file)
+roughness = div_data["roughness"].to_numpy()
 shore_height = topo["shore_height"].to_numpy()
 wall_height = topo["wall_height"].to_numpy()
-slope = topo["slope"].to_numpy()
+slope = div_data["slope"].to_numpy()
+divs = div_data["div"].to_numpy()
 
 surge = pd.read_csv(storm_file)
 surge_height = surge["height"].to_numpy()
 
 ### INPUT DATA ###
 l_seg = 100  # length of a segment
-d_seg = 1500  # depth of segment (shore to middle of LM)
 dt = 1 * 60 ** 2  # length of a time segment in seconds
-
 
 h_crit = shore_height + wall_height
 
-volume = np.zeros(surge_height.size)
-vel = np.zeros(surge_height.size)
-for div in topo["div"]:
-    h_diff = surge_height - np.tile(h_crit[div], surge_height.size)
-    vel[h_diff > 0] = ((l_seg * h_diff[h_diff > 0]) / (l_seg + 2 * h_diff[h_diff > 0])) ** (2/3) * slope[div] ** (1/2) / roughness[div]
-    vel[h_diff < 0] = 0
-    if wall_height[div] > 0:
-        cwr = 0.611 + 0.075 * h_diff / np.tile(h_crit[div], surge_height.size) 
-        vel = vel * cwr
-    volume[div] = np.sum(l_seg*dt*h_diff*vel)
+volume_div = np.zeros(divs.size)
+for div in divs:
+    volume_sub = np.zeros(surge_height.size)
+    vel = np.zeros(surge_height.size)
+    subsections = topo["subsection"].to_numpy()
+    subsections = subsections[(topo["div"] == div)]
+    print(subsections)
+    for subsection in subsections:
+        h_diff = surge_height - np.tile(h_crit[subsection], surge_height.size)
+        vel[h_diff > 0] = ((l_seg * h_diff[h_diff > 0]) / (l_seg + 2 * h_diff[h_diff > 0])) ** (2/3) * slope[div] ** (1/2) / roughness[div]
+        vel[h_diff < 0] = 0
+        if wall_height[subsection] > 0:
+            cwr = 0.611 + 0.075 * h_diff / np.tile(h_crit[subsection], surge_height.size) 
+            vel = vel * cwr
+        volume_sub[div] = np.sum(l_seg * dt * h_diff * vel)
+    volume_div[div] = np.sum(volume_sub)
 
-total_volume = np.sum(volume)
-print (total_volume)
-water_height = 2 * total_volume / (d_seg * shore_height.size * l_seg)
-print(water_height)
+print(volume_div)
